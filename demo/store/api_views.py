6181f666f5e26c358105cadb1e633920c9899254
Suraj Rimal <surajrimal50@gmail.com>
2021-08-09 22:57:50 -0500

    validation created for product create

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import ListAPIView, CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
@@ -15,9 +16,9 @@ class ProductList(ListAPIView):
     serializer_class = ProductSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
-    serach_fields = ('name', 'description')
+    serach_fields = ('name', 'description',)
     pagination_class = ProductsPagination
-    
+
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
         if on_sale is None:
@@ -31,3 +32,15 @@ class ProductList(ListAPIView):
                 sale_end__gte=now,
             )
         return queryset
+
+class ProductCreate(CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            price = request.data.get('price')
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({ 'price': 'Must be above $0.00'})
+        except ValueError:
+            raise ValidationError({ 'price': 'A valid number is required'})
+        return super().create(request, *args, **kwargs)
